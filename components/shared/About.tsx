1e3a7ef849e9d379d45e55fba0448edfb41b0270
tonymacarony-code <antonybodnev@gmail.com>
2025-01-07 14:34:53 -0800

    Fix layout

--- components/shared/About.tsx
+++ components/shared/About.tsx
@@ -7,18 +7,15 @@ import FactCards from './FactCards';
 export const About = () => {
 
     return (
-        <section id='about' className='px-6 md:px-10 mt-0 md:mt-36  overflow-hidden'>
+        <section id='about' className='px-6 md:px-10 mt-0 lg:mt-36  overflow-hidden'>
             <SectionTitle name='About' />
-            <div className="flex-col lg:flex-row lg:flex items-center justify-between">
+            <div className="flex-col  lg:flex-row flex items-center justify-center lg:justify-between">
 
-                <p className='w-full text-[6vw] leading-[6.25vw] md:w-7/12 mt-10 md:text-[3vw] md:leading-[3vw] text-pretty tracking-tighter'>
+                <p className='w-full text-[6vw] leading-[6.25vw] lg:w-7/12 mt-10 lg:text-[3vw] lg:leading-[3vw] text-pretty tracking-tighter'>
                     Front-end developer with experience in the development of fast and user-friendly applications and websites. I like to experiment, try new approaches and technologies for the implementation of projects even better.
                 </p>
-                <div className="w-full mt-10 md:mt-0  md:w-5/12 h-[600px] md:h-[650px]">
+                <div className="w-full mt-10 lg:mt-0  md:w-5/12 h-[500px] sm:h-[600px] md:h-[650px]">
                     <FactCards />
-
-
-
                 </div>
             </div>
         </section>
