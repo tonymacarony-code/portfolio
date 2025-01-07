e7da1ce02bd5051a2fcb8dc1315b0e372a030450
tonymacarony-code <antonybodnev@gmail.com>
2025-01-07 14:05:30 -0800

    Fix tablet about

--- components/shared/About.tsx
+++ components/shared/About.tsx
@@ -9,7 +9,7 @@ export const About = () => {
     return (
         <section id='about' className='px-6 md:px-10 mt-0 md:mt-36  overflow-hidden'>
             <SectionTitle name='About' />
-            <div className="flex-col md:flex-row md:flex items-center justify-between">
+            <div className="flex-col lg:flex-row lg:flex items-center justify-between">
 
                 <p className='w-full text-[6vw] leading-[6.25vw] md:w-7/12 mt-10 md:text-[3vw] md:leading-[3vw] text-pretty tracking-tighter'>
                     Front-end developer with experience in the development of fast and user-friendly applications and websites. I like to experiment, try new approaches and technologies for the implementation of projects even better.
