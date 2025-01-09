a73e837003166d9cc1eb75c0e485dab4790f3812
tonymacarony-code <antonybodnev@gmail.com>
2025-01-09 12:09:00 -0800

    test build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import './drag-animation.css';
-import Analytics from "@/components/shared/Analytics";
+import Script from "next/script";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -21,7 +21,16 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <head>
-        <Analytics />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-C265KL3GEY" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+   
+             gtag('config', 'G-C265KL3GEY');
+             `}
+        </Script>
       </head>
       <body
         className={`${manrope.className}  antialiased scroll-smooth`}
